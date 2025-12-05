76c39a7c48c0e45e77c7de1a0c6fd1391ab6b42a
BAIZIKING <yijin2@andrew.cmu.edu>
2025-12-05 14:58:39 -0500

    fix

--- main.py
+++ main.py
@@ -14,56 +14,64 @@ from fuzzy import FuzzyAlgo
 from surprise import SVD, BaselineOnly, KNNBasic
 
 
-# matrix factorisation baseline methods
-baseline = BaselineOnly()
-svd = SVD()
 
-# collaborative filtering baseline method
-knn = KNNBasic()
+FUZZY_RANDOM_SEED = 10701
+FUZZY_NUM_CLUSTERS = 8
+FRIENDS_K = 20
+COMBINATION_COEFF_C = 0.5
 
-#partial cold start
-trainset, testset = cold_start_train()
+if __name__ == "__main__":
 
-print("Baseline Models")
-baseline.fit(trainset)
-svd.fit(trainset)
-knn.fit(trainset)
-print("Baseline Models training complete")
-
-
-print("RMSE \t MAE \t Precision@10\t Recall@10")
-print('BaselineOnly', evaluate(testset, baseline))
-print('SVD', evaluate(testset, svd))
-print('kNN, k=40',evaluate(testset, knn))
-
-print("----------------------------")
-print("Basic Models")
-print()
-print("Training RDFSVD")
-rdfsvd = RDFSVD(trainset.n_users, trainset.n_items)
-rdfsvd.fit(trainset)
-print("Training complete")
-print("RMSE \t MAE \t Precision@10\t Recall@10")
-print('RDFSVD', evaluate(testset, rdfsvd))
-print()
-print("Training Cluster")
-c = Cluster(trainset.n_users, trainset.n_items)
-c.fit(trainset)
-print("Training complete")
-print("RMSE \t MAE \t Precision@10\t Recall@10")
-print('Cluster', evaluate(testset, c))
-print()
-print("Training Fuzzy")
-fuzzy = FuzzyAlgo(trainset.n_users, trainset.n_items)
-fuzzy.fit(trainset)
-print("Training complete")
-print("RMSE \t MAE \t Precision@10\t Recall@10")
-print('RDFSVD', evaluate(testset, fuzzy))
-
-print("----------------------------")
-print('Ensemble: each basic model applied 3 times, with bagging')
-alg = Ensemble()
-alg.fit(trainset)
-print("Training complete")
-print("RMSE \t MAE \t Precision@10\t Recall@10")
-print('Ensemble', evaluate(testset, alg))
+    # matrix factorisation baseline methods
+    baseline = BaselineOnly()
+    svd = SVD()
+    
+    # collaborative filtering baseline method
+    knn = KNNBasic()
+    
+    #partial cold start
+    trainset, testset = cold_start_train()
+    
+    print("Baseline Models")
+    baseline.fit(trainset)
+    svd.fit(trainset)
+    knn.fit(trainset)
+    print("Baseline Models training complete")
+    
+    
+    print("RMSE \t MAE \t Precision@10\t Recall@10")
+    print('BaselineOnly', evaluate(testset, baseline))
+    print('SVD', evaluate(testset, svd))
+    print('kNN, k=40',evaluate(testset, knn))
+    
+    print("----------------------------")
+    print("Basic Models")
+    print()
+    print("Training RDFSVD")
+    rdfsvd = RDFSVD(trainset.n_users, trainset.n_items)
+    rdfsvd.fit(trainset)
+    print("Training complete")
+    print("RMSE \t MAE \t Precision@10\t Recall@10")
+    print('RDFSVD', evaluate(testset, rdfsvd))
+    print()
+    print("Training Cluster")
+    c = Cluster(trainset.n_users, trainset.n_items)
+    c.fit(trainset)
+    print("Training complete")
+    print("RMSE \t MAE \t Precision@10\t Recall@10")
+    print('Cluster', evaluate(testset, c))
+    print()
+    print("Training Fuzzy")
+    fuzzy = FuzzyAlgo(trainset.n_users, trainset.n_items)
+    fuzzy.fit(trainset)
+    print("Training complete")
+    print("RMSE \t MAE \t Precision@10\t Recall@10")
+    print('RDFSVD', evaluate(testset, fuzzy))
+    
+    print("----------------------------")
+    print('Ensemble: each basic model applied 3 times, with bagging')
+    alg = Ensemble()
+    alg.fit(trainset)
+    print("Training complete")
+    print("RMSE \t MAE \t Precision@10\t Recall@10")
+    print('Ensemble', evaluate(testset, alg))
